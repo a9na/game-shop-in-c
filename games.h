a9fb27e18d6c7fe8084fe1e22e70a4683cb0a03c
Ana Novković <105394294+a9na@users.noreply.github.com>
2023-05-31 22:25:14 +0200

    Update games.h

--- games.h
+++ games.h
@@ -4,9 +4,9 @@
 #define MAX_GAMES 15
 
 typedef struct {
-	char title[50];
-	char genre[50];
-	float price;
+    char title[50];
+    char genre[50];
+    float price;
 } Game;
 
 void printMainMenu();
@@ -16,4 +16,5 @@ void searchByGenre(Game** games, int* gameCount);
 void initializeGames(Game** games, int* gameCount);
 void saveGameToFile(Game game);
 
-#endif  // GAMES_H
+#endif // GAMES_H
+
