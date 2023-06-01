1e0968e803f638b47abf350bb6babe3f37878b62
Ana Novković <105394294+a9na@users.noreply.github.com>
2023-06-01 17:14:18 +0200

    Update games.c

--- games.c
+++ games.c
@@ -1,11 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+//1 6
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include "games.h"
 #include <ctype.h>
+
+// 8
 
 void printMainMenu() {
 	printf("Main Menu:\n");
@@ -14,7 +18,7 @@ void printMainMenu() {
 	printf("3. Exit\n");
 }
 
-static void printGenreMenu() {
+static void printGenreMenu() {      // 5
 	printf("Genre Menu:\n");
 	printf("0. Back\n");
 	printf("1. Action-Adventure\n");
@@ -32,6 +36,8 @@ static void printGameMenu() {
 	printf("3. Purchase\n");
 }
 
+// 9  10  11   12
+
 int compareGames(const void* a, const void* b) {
 	Game* gameA = (Game*)a;
 	Game* gameB = (Game*)b;
@@ -41,7 +47,7 @@ int compareGames(const void* a, const void* b) {
 void sortGames(GameCollection* collection) {
 	GameNode* games = collection->head;
 	int gameCount = collection->count;
-	Game* gameArray = (Game*)malloc(sizeof(Game) * gameCount);
+	Game* gameArray = (Game*)malloc(sizeof(Game) * gameCount);   // 13 14   
 	if (gameArray == NULL) {
 		printf("Memory allocation failed.\n");
 		return;
@@ -52,7 +58,7 @@ void sortGames(GameCollection* collection) {
 		games = games->next;
 	}
 
-	qsort(gameArray, gameCount, sizeof(Game), compareGames);
+	qsort(gameArray, gameCount, sizeof(Game), compareGames);   //20
 
 	games = collection->head;
 	for (int i = 0; i < gameCount; i++) {
@@ -60,11 +66,11 @@ void sortGames(GameCollection* collection) {
 		games = games->next;
 	}
 
-	free(gameArray);
+	free(gameArray);   // 15
 }
 
 void renameGameFile(const char* oldName, const char* newName) {
-	int status = rename(oldName, newName);
+	int status = rename(oldName, newName);    //18
 	if (status == 0) {
 		printf("File '%s' renamed to '%s'.\n", oldName, newName);
 	}
@@ -72,11 +78,24 @@ void renameGameFile(const char* oldName, const char* newName) {
 		printf("Error renaming file '%s' to '%s': %s\n", oldName, newName, strerror(errno));
 	}
 }
-
+// 16 17 19
 void saveGameToFile(Game game) {
 	FILE* file = fopen("games.txt", "a");
 	if (file == NULL) {
 		printf("Error opening file for saving game: %s\n", strerror(errno));
+		return;
+	}
+
+	if (fseek(file, 0, SEEK_END) != 0) {
+		printf("Error seeking to the end of the file: %s\n", strerror(errno));
+		fclose(file);
+		return;
+	}
+
+	long fileSize = ftell(file);
+	if (fileSize == -1) {
+		printf("Error getting file position: %s\n", strerror(errno));
+		fclose(file);
 		return;
 	}
 
@@ -86,6 +105,7 @@ void saveGameToFile(Game game) {
 
 	fclose(file);
 }
+
 
 void searchByGenre(GameCollection* collection) {
 	printGenreMenu();
@@ -184,43 +204,54 @@ void searchByGenre(GameCollection* collection) {
 	sortGames(collection);
 	renameGameFile("games.txt", "games_new.txt");
 }
-void searchByTitle(GameCollection* collection) {
+void searchByTitleBinary(GameCollection* collection) {
 	char searchTitle[MAX_TITLE_LENGTH];
 	printf("Enter the game title: ");
 	fgets(searchTitle, sizeof(searchTitle), stdin);
 	searchTitle[strcspn(searchTitle, "\n")] = '\0';
 
+	Game key;
+	strcpy(key.title, searchTitle);
+
 	GameNode* games = collection->head;
-	int gameFound = 0;
-	int gameIndex = 0;
+	size_t gameCount = collection->count;
 
-	for (int i = 0; searchTitle[i]; i++) {
-		searchTitle[i] = tolower(searchTitle[i]);
-	}
+	sortGames(collection);
 
-	while (games != NULL) {
-	
-		char gameTitle[MAX_TITLE_LENGTH];
-		strcpy(gameTitle, games->data.title);
-		for (int i = 0; gameTitle[i]; i++) {
-			gameTitle[i] = tolower(gameTitle[i]);
-		}
+	Game* result = (Game*)bsearch(&key, games, gameCount, sizeof(Game), compareGames);    //21
+	if (result != NULL) {
+		printf("Game found: %s\n", result->title);
+		printGameMenu();
 
-		if (strcmp(gameTitle, searchTitle) == 0) {
-			printf("Available!\n");
-			gameFound = 1;
+		int menuChoice;
+		printf("Enter your choice: ");
+		scanf("%d", &menuChoice);
+		getchar();
+
+		switch (menuChoice) {
+		case 0:
+			break;
+		case 1:
+			printf("Added \"%s\" to the cart.\n", result->title);
+			break;
+		case 2:
+			printf("Added \"%s\" to the wishlist.\n", result->title);
+			break;
+		case 3:
+			saveGameToFile(*result);
+			printf("Purchased \"%s\". The game has been saved to games.txt.\n", result->title);
+			break;
+		default:
+			printf("Invalid choice. Please try again.\n");
 			break;
 		}
-		games = games->next;
-		gameIndex++;
 	}
-
-	if (!gameFound) {
+	else {
 		printf("Game not found.\n");
 	}
 }
 
-
+// 4
 void initializeGames(GameCollection* collection) {
 	collection->count = MAX_GAMES;
 	collection->head = NULL;
@@ -260,13 +291,12 @@ void initializeGames(GameCollection* collection) {
 	}
 }
 
-
 void freeGames(GameCollection* collection) {
 	GameNode* games = collection->head;
 	while (games != NULL) {
 		GameNode* current = games;
 		games = games->next;
-		free(current);
+		free(current);   
 	}
 	collection->count = 0;
 	collection->head = NULL;
