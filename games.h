34cb23db096e0eae119a25cd3b6d07dd2c95b917
Ana Novković <105394294+a9na@users.noreply.github.com>
2023-06-01 15:34:31 +0200

    Update games.h

--- games.h
+++ games.h
@@ -2,19 +2,40 @@
 #define GAMES_H
 
 #define MAX_GAMES 15
+#define MAX_TITLE_LENGTH 50
+
+typedef enum {
+	ACTION_ADVENTURE,
+	RPG,
+	FPS,
+	PUZZLE,
+	SPORTS
+} Genre;
 
 typedef struct {
-    char title[50];
-    char genre[50];
-    float price;
+	char title[MAX_TITLE_LENGTH];
+	Genre genre;
+	float price;
 } Game;
+
+typedef struct GameNode {
+	Game data;
+	struct GameNode* next;
+} GameNode;
+
+typedef struct {
+	GameNode* head;
+	int count;
+} GameCollection;
 
 void printMainMenu();
 void printGenreMenu();
 void printGameMenu();
-void searchByGenre(Game** games, int* gameCount);
-void initializeGames(Game** games, int* gameCount);
+void searchByGenre(GameCollection* collection);
+void searchByTitle(GameCollection* collection);
+void initializeGames(GameCollection* collection);
 void saveGameToFile(Game game);
+void sortGames(GameCollection* collection);
+void renameGameFile(const char* oldName, const char* newName);
 
 #endif // GAMES_H
-
